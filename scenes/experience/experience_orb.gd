3c41b28a54d15100b38ce9fb6995dd71808ff5a1
Franco G <francogiannotti115@gmail.com>
2025-12-25 03:42:29 -0300

    add boucne to the xp

--- scenes/experience/experience_orb.gd
+++ scenes/experience/experience_orb.gd
@@ -1,41 +1,51 @@
 extends RigidBody3D
 
+enum State { IDLE, BOUNCING, CHASING }
+
 @export var xp_value = 1
-@export var flight_speed = 0.5
+@export var flight_speed = 2
+@export var bounce_back_force = 2
+@export var bounce_back_duration = 0.25
+@export var acceleration_rate = 40
 
 var target_player: ProtoController = null
+var state = State.IDLE
 
 func _physics_process(delta):
-	if target_player:
-		# 1. Turn off normal physics so we can fly freely
-		gravity_scale = 0
-		# Turn off collision so it doesn't get stuck on walls
-		collision_layer = 0 
+	if state != State.CHASING:
+		return
 		
-		# 2. Fly towards the player
-		var direction = (target_player.global_position - global_position).normalized()
-		linear_velocity = direction * flight_speed
-		
-		# 3. Accelerate over time for a snappy feel
-		flight_speed += 3 * delta 
+	gravity_scale = 0
+	collision_layer = 0 
+	chase(delta)
 
-		# 4. Check if we reached the player (Pickup)
-		var distance = global_position.distance_to(target_player.global_position)
-		if distance < 1.0:
-			collect()
-
-# Called by the Player's magnet area
 func magnetize(player_node):
+	if target_player:
+		return
 	target_player = player_node
-	# Give it a little pop up before flying
-	linear_velocity.y += 2.0 
+	state = State.BOUNCING
+	bounce(player_node)
+	
+	await get_tree().create_timer(bounce_back_duration).timeout
+	state = State.CHASING
 
 func collect():
-	print("Collected XP!")
 	target_player.xp_component.add_xp(xp_value)
 	queue_free()
-
 
 func _on_pickup_range_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		magnetize(body)
+
+func bounce(player):
+	var away_direction = (global_position - player.global_position).normalized()
+	away_direction.y = 0.5
+	linear_velocity = away_direction * bounce_back_force
+
+func chase(delta):
+	var direction = (target_player.global_position - global_position).normalized()
+	linear_velocity = direction * flight_speed
+	flight_speed *= pow(acceleration_rate, delta)
+
+	if global_position.distance_to(target_player.global_position) < 1.0:
+		collect()
