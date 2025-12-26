63dd83511f959b24eb65b314f2802a3b84504d1c
Martin Fraga <fragamartine@gmail.com>
2025-12-26 11:22:56 -0300

    add projectile

--- scripts/hitbox_component.gd
+++ scripts/hitbox_component.gd
@@ -1,5 +1,10 @@
 class_name HitboxComponent
 extends Area3D
 
+signal on_hit(target: HurtboxComponent)
+
 @export_category("stats")
 @export var damage_amount: float
+
+func register_hit(hurtbox: HurtboxComponent) -> void:
+	on_hit.emit(hurtbox)
---
+++ scripts/projectile.gd
@@ -0,0 +1,19 @@
+class_name Projectile
+extends Node3D
+
+@export_group("References")
+@export var hitbox: HitboxComponent
+
+@export_group("Travel Stats")
+@export var projectile_speed : float
+
+var direction : Vector3
+
+func _ready() -> void:
+	hitbox.on_hit.connect(_on_projectile_hit)
+
+func _physics_process(delta: float) -> void:
+	position += direction * delta * projectile_speed
+
+func _on_projectile_hit() -> void:
+	call_deferred("queue_free")
