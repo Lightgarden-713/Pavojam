2b98ffc10835f235fbadf3525b88c4dd0e011d60
Martin Fraga <fragamartine@gmail.com>
2025-12-29 23:26:27 -0300

    fix multiple projectile support

--- scripts/projectile_attack.gd
+++ scripts/projectile_attack.gd
@@ -13,7 +13,7 @@ enum AttackMode { AIMED, AUTO_AIMED }
 @export var projectile_speed: float
 @export var attack_mode: AttackMode
 @export var attacks_per_second: float
-@export var projectiles_per_attack: float
+@export var projectiles_per_attack: int
 
 @export_group("Projectile Config")
 @export_flags_3d_physics var projectile_collision_mask : int
@@ -33,48 +33,52 @@ func _process(delta: float) -> void:
 		shoot()
 
 func shoot() -> void:
-	# Pick target as a forward pos
-	var target = global_position - global_transform.basis.z
+	var entities_in_range : Array[Node3D] = []
+	entities_in_range.append_array(entity_tracker.entities_within_detection_range)
 
-	if attack_mode == AttackMode.AUTO_AIMED:
-		var entities_in_range : Array[Node3D] = entity_tracker.entities_within_detection_range
+	for projectile_n in range(projectiles_per_attack):
+		# Pick target as a forward pos
+		var target = global_position - global_transform.basis.z
 
-		if entities_in_range.is_empty():
-			# Reset shoot timer
-			time_until_shooting = 1 / current_attacks_per_second
-			return
+		if attack_mode == AttackMode.AUTO_AIMED:
 
-		# get closest entity
-		var closest_entity = entities_in_range[0]
-		for entity in entities_in_range:
-			if global_position.distance_to(entity.global_position) < global_position.distance_to(closest_entity.global_position):
-				closest_entity = entity
+			if entities_in_range.is_empty():
+				# Reset shoot timer
+				time_until_shooting = 1 / current_attacks_per_second
+				return
 
-		target = closest_entity.global_position
+			# get closest entity
+			var closest_entity = entities_in_range[0]
+			for entity in entities_in_range:
+				if global_position.distance_to(entity.global_position) < global_position.distance_to(closest_entity.global_position):
+					closest_entity = entity
 
-	# Calculate direction towards target
-	var shoot_direction = global_position.direction_to(target)
+			target = closest_entity.global_position
 
-	spawn_projectiles(shoot_direction)
+			# Can only target once per shooting
+			entities_in_range.erase(closest_entity)
+
+		# Calculate direction towards target
+		var shoot_direction = global_position.direction_to(target)
+
+		spawn_projectile(shoot_direction)
 
 	# Reset shoot timer
 	time_until_shooting = 1 / current_attacks_per_second
 
-func spawn_projectiles(main_projectile_direction: Vector3) -> void:
+func spawn_projectile(projectile_direction: Vector3) -> void:
 	var tree_root = get_tree().root
 
-	# Spawn and setup Projectiles
-	for projectile_n in range(projectiles_per_attack):
-		var projectile : Projectile = projectile_prefab.instantiate() as Projectile
+	var projectile : Projectile = projectile_prefab.instantiate() as Projectile
 
-		projectile.hitbox.damage_amount = projectile_damage
-		projectile.hitbox.collision_mask = projectile_collision_mask
+	projectile.hitbox.damage_amount = projectile_damage
+	projectile.hitbox.collision_mask = projectile_collision_mask
 
-		projectile.projectile_speed = projectile_speed
-		projectile.direction = main_projectile_direction
-		projectile.position = global_position
+	projectile.projectile_speed = projectile_speed
+	projectile.direction = projectile_direction
+	projectile.position = global_position
 
-		# Vector3(0, 0.7, -0.7)  →  atan2()  →  Rotation Angle (≈ -45°)
-		projectile.rotation.y = atan2(main_projectile_direction.x, main_projectile_direction.z)
+	# Vector3(0, 0.7, -0.7)  →  atan2()  →  Rotation Angle (≈ -45°)
+	projectile.rotation.y = atan2(projectile_direction.x, projectile_direction.z)
 
-		tree_root.call_deferred("add_child", projectile)
+	tree_root.call_deferred("add_child", projectile)
