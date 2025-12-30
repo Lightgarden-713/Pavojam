bfde1e070badf7da363b11ae83db50ffba1d736e
Martin Fraga <fragamartine@gmail.com>
2025-12-30 01:06:22 -0300

    make attack projectiles not collide with the level, add lifetime to projectiles

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -6,11 +6,19 @@ extends Node3D
 
 @export_group("Travel Stats")
 @export var projectile_speed : float
+@export var projectile_lifetime_sec: float
 
 var direction : Vector3
+var projectile_time_left : float
 
 func _ready() -> void:
 	hitbox.on_hit.connect(_on_projectile_hit)
+	projectile_time_left = projectile_lifetime_sec
+
+func _process(delta: float) -> void:
+	projectile_time_left -= delta
+	if projectile_time_left <= 0:
+		call_deferred("queue_free")
 
 func _physics_process(delta: float) -> void:
 	position += direction * delta * projectile_speed
