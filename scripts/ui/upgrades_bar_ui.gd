e98db9b7e64f041ff925d1485147afdfa915b150
Franco G <francogiannotti115@gmail.com>
2026-01-03 18:49:54 -0300

    handle separation decrements

--- scripts/ui/upgrades_bar_ui.gd
+++ scripts/ui/upgrades_bar_ui.gd
@@ -15,3 +15,22 @@ func _on_level_up_finish() -> void:
 	upgrade_rect.texture = level_up_ui.selected_upgrade.image
 	upgrade_rect.custom_minimum_size = Vector2(64, 64)
 	upgrades_container.add_child(upgrade_rect)
+	var childs = upgrades_container.get_child_count()
+	
+	if childs > 6 and childs < 9:
+		upgrades_container.add_theme_constant_override("separation", -36)
+
+	if childs > 8 and childs < 11:
+		upgrades_container.add_theme_constant_override("separation", -40)
+
+	if childs > 12:
+		upgrades_container.add_theme_constant_override("separation", -48)
+
+	if childs > 16:
+		upgrades_container.add_theme_constant_override("separation", -52)
+
+	if childs > 20:
+		upgrades_container.add_theme_constant_override("separation", -54)
+
+	if childs > 24:
+		upgrades_container.add_theme_constant_override("separation", -56)
